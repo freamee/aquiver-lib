0f27ae8d7e1602601dfb44cd885ed17ffb1977c4
Krisztián S <sutyakkrisztian999@hotmail.com>
2023-04-13 06:30:11 +0200

    push

--- src_lua/shared/eventhandler/evenhandler.lua
+++ src_lua/shared/eventhandler/evenhandler.lua
@@ -11,6 +11,8 @@ EventHandler.new = function()
     return self
 end
 
+--- This event only added in the current resource.
+--- It can pass the metatables in the arguments bypassing the fivem AddEventHandler
 ---@param eventName string
 ---@param cb fun(...)
 function EventHandler:AddEvent(eventName, cb)
@@ -18,9 +20,15 @@ function EventHandler:AddEvent(eventName, cb)
         self.events[eventName] = {}
     end
 
+    _G.APIShared.Helpers.Logger:debug(
+        string.format("Registered new event: %s", eventName)
+    )
+
     self.events[eventName][#self.events[eventName] + 1] = cb
 end
 
+--- This function only trigger on local. (so server->server and client->client)
+--- It can pass the metatables in the arguments bypassing the fivem AddEventHandler
 ---@param eventName string
 ---@param ... any
 function EventHandler:TriggerEvent(eventName, ...)
