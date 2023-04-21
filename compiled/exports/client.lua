6d047bac0d410654de6dec6fd16e8461da32f8c2
Krisztián S <sutyakkrisztian999@hotmail.com>
2023-04-22 01:46:57 +0200

    player setHeading, test server.cfg config

--- compiled/exports/client.lua
+++ compiled/exports/client.lua
@@ -1886,6 +1886,11 @@ local CONFIG = {}
 CONFIG.DebugEnabled = false
 CONFIG.AQUIVER_TEST_SERVER = false -- Do not mess with this variable, this one is for us for the test server.
 
+if GetConvar("avp_test_server", 'false') == "true" then
+    CONFIG.AQUIVER_TEST_SERVER = true
+    print("^3Aquiver Products Test Server recognized!")
+end
+
 return CONFIG
 
 end)
